c2cf636b2b455a78331aca333e671b806152b71c
MinhQuan-themga <qnguyenminh195@gmail.com>
2025-03-10 14:32:44 +0700

    hoanThanhTask

--- src/view/LoginForm.java
+++ src/view/LoginForm.java
@@ -15,6 +15,8 @@ public class LoginForm extends javax.swing.JFrame {
      */
     public LoginForm() {
         initComponents();
+        System.out.println("Đã làm backend");
+        System.out.println("huhu haha");
     }
 
     /**

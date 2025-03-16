e2a7977417b5fb0495df8b4f80bfdc383a246c10
thanhthuy243 <thuy30398@gmail.com>
2025-03-16 22:19:18 +0700

    them 1 day so

--- src/view/loginThanhThuy.java
+++ src/view/loginThanhThuy.java
@@ -15,7 +15,7 @@ public class loginThanhThuy extends javax.swing.JFrame {
      */
     public loginThanhThuy() {
         initComponents();
-        System.out.println("thanhthuy 123");
+        System.out.println("thanhthuy 1234567");
     }
 
     /**
